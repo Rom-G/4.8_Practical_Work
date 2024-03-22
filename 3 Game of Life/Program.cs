33872c2496e289db9e1bd5b07001b0346d003470
Rom-G <rom-goldberg@ya.ru>
2024-03-22 19:44:41 +0700

    Добавьте файлы проекта.

---
+++ 3 Game of Life/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Game_of_Life
+{
+    public class LifeSimulation
+    {
+        private int _heigth;
+        private int _width;
+        private bool[,] cells;
+        private bool[,] cellsBuffer;
+
+        /// <summary>
+        /// Создаем новую игру
+        /// </summary>
+        /// <param name="Heigth">Высота поля.</param>
+        /// <param name="Width">Ширина поля.</param>
+
+        public LifeSimulation(int Heigth, int Width)
+        {
+            _heigth = Heigth;
+            _width = Width;
+            cells = new bool[Heigth, Width];
+            cellsBuffer = new bool[Heigth, Width];
+            GenerateField();
+        }
+
+        /// <summary>
+        /// Перейти к следующему поколению и вывести результат на консоль.
+        /// </summary>
+        public void DrawAndGrow()
+        {
+            DrawGame();
+            Grow();
+        }
+
+        /// <summary>
+        /// Двигаем состояние на одно вперед, по установленным правилам
+        /// </summary>
+
+        private void Grow()
+        {
+            for (int i = 0; i < _heigth; i++)
+            {
+                for (int j = 0; j < _width; j++)
+                {
+                    int numOfAliveNeighbors = GetNeighbors(i, j);
+
+                    if (cells[i, j])
+                    {
+                        if (numOfAliveNeighbors - 1 < 2)
+                        {
+                            cellsBuffer[i, j] = false;
+                        }
+
+                        if (numOfAliveNeighbors - 1 > 3)
+                        {
+                            cellsBuffer[i, j] = false;
+                        }
+                    }
+                    else
+                    {
+                        if (numOfAliveNeighbors == 3)
+                        {
+                            cellsBuffer[i, j] = true;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < _heigth; i++)
+            {
+                for (int j = 0; j < _width; j++)
+                {
+                    cells[i, j] = cellsBuffer[i, j];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Смотрим сколько живых соседей вокруг клетки.
+        /// </summary>
+        /// <param name="x">X-координата клетки.</param>
+        /// <param name="y">Y-координата клетки.</param>
+        /// <returns>Число живых клеток.</returns>
+
+        private int GetNeighbors(int x, int y)
+        {
+            int NumOfAliveNeighbors = 0;
+
+            for (int i = x - 1; i < x + 2; i++)
+            {
+                for (int j = y - 1; j < y + 2; j++)
+                {
+                    if (!((i < 0 || j < 0) || (i >= _heigth || j >= _width)))
+                    {
+                        if (cells[i, j] == true) NumOfAliveNeighbors++;
+                    }
+                }
+            }
+            return NumOfAliveNeighbors;
+        }
+
+        /// <summary>
+        /// Нарисовать Игру в консоле
+        /// </summary>
+
+        private void DrawGame()
+        {
+            Console.CursorVisible = false;
+            for (int i = 0; i < _heigth; i++)
+            {
+                for (int j = 0; j < _width; j++)
+                {
+                    Console.Write(cells[i, j] ? "\u263B" : " ");
+                    if (j == _width - 1) Console.WriteLine("\r");
+                }
+            }
+            Console.SetCursorPosition(0, Console.WindowTop);
+        }
+
+        /// <summary>
+        /// Инициализируем случайными значениями
+        /// </summary>
+
+        private void GenerateField()
+        {
+            Random generator = new Random();
+            int number;
+            for (int i = 0; i < _heigth; i++)
+            {
+                for (int j = 0; j < _width; j++)
+                {
+                    number = generator.Next(11);
+                    cells[i, j] = ((number > 0) ? false : true);
+                    cellsBuffer[i, j] = cells[i, j];
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+
+        // Ограничения игры
+        private const int Heigth = 47;
+        private const int Width = 159;
+        private const uint MaxRuns = 1000;
+
+        private static void Main(string[] args)
+        {
+            int runs = 0;
+            LifeSimulation sim = new LifeSimulation(Heigth, Width);
+
+            while (runs++ < MaxRuns)
+            {
+                sim.DrawAndGrow();
+
+                // Дадим пользователю шанс увидеть, что происходит, немного ждем
+                System.Threading.Thread.Sleep(75);
+            }
+        }
+    }
+}
+
---
+++ 4.8_Practical_work/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._8_Practical_work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, давайте создадим матрицу...");
+            Console.Write("Введите количество строк: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество столбцов: ");
+            int col = int.Parse(Console.ReadLine());
+
+            int[,] matrix1 = new int[row, col];
+            int[,] matrix2 = new int[row, col];
+            int[,] matrixSum = new int[row, col];
+            int sum = 0;
+
+            Random r = new Random();
+
+            for (int i = 0; i < row; i++) 
+            {
+                for (int j = 0; j < col; j++) 
+                {
+                    matrix1[i, j] = r.Next(10);
+                    matrix2[i, j] = r.Next(10);
+                    matrixSum[i, j] = matrix1[i, j] + matrix2[i, j];
+                    sum += matrix1[i, j];
+                    Console.Write($"{matrix1[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Сумма всех элементов матрицы составит: {sum}.");
+            Console.WriteLine("\nТеперь создадим ещё одну матрицу того же размера и выполним их сложение.");
+            Console.Write("Для продолжения нажмите Enter...");
+            Console.ReadLine();
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write($"{matrix1[i, j]} ");
+                }
+
+                if ((row % 2 == 0 && i == row / 2 - 1) || (row % 2 != 0 && i == row / 2))
+                {
+                    Console.Write(" +  ");
+                }
+                else 
+                {
+                    Console.Write("    ");
+                }
+
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write($"{matrix2[i, j]} ");
+                }
+
+                if ((row % 2 == 0 && i == row / 2 - 1) || (row % 2 != 0 && i == row / 2))
+                {
+                    Console.Write(" =  ");
+                }
+                else
+                {
+                    Console.Write("    ");
+                }
+
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write($"{matrixSum[i, j], 2} ");
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
